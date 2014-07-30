a5efb9c4ccf1d8bde8317cdcb8c87ae67e53e5a5
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-07-30 13:07:01 +0900

    Refactors and fixes preset bug

--- ObamaApp/ObamaChair/OBCMainViewController.m
+++ ObamaApp/ObamaChair/OBCMainViewController.m
@@ -32,9 +32,7 @@ NSTimer *rssiTimer;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
 
-    
     self.color = [[NSArray alloc] init];
 
     self.view.backgroundColor = [UIColor whiteColor];
@@ -47,26 +45,20 @@ NSTimer *rssiTimer;
     [self.ble controlSetup];
     self.ble.delegate = self;
     self.preset_one_set = false;
-    self.distance_preset_one = 0.0f;
+    
     [self initAudioPlayer];
-    
-    
+       
     UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(obamaWasTapped)];
     [self.faceImageContainer addGestureRecognizer:singleTap];
     [self.faceImageContainer setMultipleTouchEnabled:YES];
     [self.faceImageContainer setUserInteractionEnabled:YES];
-    // Do any additional setup after loading the view.
-    
-
-    
 }
 
+
+
 -(void)obamaWasTapped{
-    NSLog(@"YOU TOUCHED OBAMAS FACE!");
     [self connectBLE];
     [self rotateFaceImageWithAtSpeed:1.0f fromAngle:0 toAngle:2*M_PI andRepeat:INFINITY];
-//    [self rotateFaceImageWithAtSpeed: 5.0f]
-//    [self rotateFaceImageWithAtSpeed:5.0f];//    self.btnConnect.backgroundColor = [UIColor redColor];
 }
 
 
@@ -82,11 +74,7 @@ NSTimer *rssiTimer;
 
 
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 /*
 #pragma mark - Navigation
@@ -132,61 +120,16 @@ NSTimer *rssiTimer;
 
 }
 
--(void) initAudioPlayer{
-    NSString *soundFilePath = [NSString stringWithFormat:@"%@/cry2.wav", [[NSBundle mainBundle] resourcePath]];
-    
-    NSLog(@"%@",soundFilePath);
-    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
-    
-    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
-    [[AVAudioSession sharedInstance] setActive: YES error: nil];
-    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
-    
-    [self.audioPlayer setVolume:1.0];
-}
-
-- (void)playAudio {
-//    [self playSound:@"cry1" :@"wav"];
-//
-    
-    
-    
-//    [self.audioPlayer stop];
-//    [self.audioPlayer setCurrentTime:0];
-    [self.audioPlayer play];
-//
-//    
-}
-
-- (void)playSound :(NSString *)fName :(NSString *) ext{
-    SystemSoundID audioEffect;
-    NSString *path = [[NSBundle mainBundle] pathForResource : fName ofType :ext];
-    if ([[NSFileManager defaultManager] fileExistsAtPath : path]) {
-        NSURL *pathURL = [NSURL fileURLWithPath: path];
-        AudioServicesCreateSystemSoundID((__bridge CFURLRef) pathURL, &audioEffect);
-        AudioServicesPlaySystemSound(audioEffect);
-    }
-    else {
-        NSLog(@"error, file not found: %@", path);
-    }
-}
 
 
--(void) readRSSITimer:(NSTimer *)timer
-{
-    [ble readRSSI];
-}
+
+
 
 - (void)bleDidDisconnect
 {
     NSLog(@"->Disconnected");
     [self.btnPresetOne setTitle:@"" forState:UIControlStateNormal];
     [self.faceImageContainer.layer removeAllAnimations];
-//    [self rotateFaceImageWithAtSpeed:100.0f];
-    
-//    [btnConnect setTitle:@"Connect" forState:UIControlStateNormal];
-//    self.faceImageContainer.image = nil;
 
     [self.distanceTimer invalidate];
     self.lblGreetingButton.alpha = 1;
@@ -212,14 +155,13 @@ NSTimer *rssiTimer;
     self.faceImageContainer.image = [UIImage imageNamed:@"ObamaCutout2.png"];
     [self.faceImageContainer.layer removeAllAnimations];
     [self.btnPresetOne setTitle:@"Remember" forState:UIControlStateNormal];
-//    [self rotateFaceImageWithAtSpeed:1 fromAngle:0 toAngle:-M_PI/2 andRepeat:1];
-//    [self rotateFaceImageWithAtSpeed:1 fromAngle:-M_PI/2 toAngle:M_PI/2 andRepeat:1];
-//    [self rotateFaceImageWithAtSpeed:1 fromAngle:M_PI/2 toAngle:0 andRepeat:1];
-    NSLog(@"->Connected");
-   self.distanceTimer = [NSTimer scheduledTimerWithTimeInterval:(float)0.1 target:self selector:@selector(checkDistanceForPreset) userInfo:nil repeats:YES];
-//    self.faceImageContainer.image = [UIImage imageNamed:@"ObamaCutout2.png"];
-    self.faceImageContainer.alpha = 1;
+
+    self.distanceTimer = [NSTimer scheduledTimerWithTimeInterval:(float)0.1 target:self selector:@selector(checkDistanceForPreset) userInfo:nil repeats:YES];
     
+    self.distance_preset_one = 0.0f;
+    self.preset_one_set = false;
+    
+    self.faceImageContainer.alpha = 1;
     self.lblGreetingButton.alpha = 0;
     self.lblGreetingTop.alpha = 0;
     
@@ -229,7 +171,7 @@ NSTimer *rssiTimer;
     [ble write:data];
     
     // Schedule to read RSSI every 1 sec.
-//    rssiTimer = [NSTimer scheduledTimerWithTimeInterval:(float)1.0 target:self selector:@selector(readRSSITimer:) userInfo:nil repeats:YES];
+    // rssiTimer = [NSTimer scheduledTimerWithTimeInterval:(float)1.0 target:self selector:@selector(readRSSITimer:) userInfo:nil repeats:YES];
 
 
 }
@@ -240,7 +182,6 @@ NSTimer *rssiTimer;
     return distance;
 }
 
-// When data is comming, this will be called
 -(void) bleDidReceiveData:(unsigned char *)data length:(int)length
 {
     for (int i = 0; i < length; i+=4){
@@ -251,22 +192,21 @@ NSTimer *rssiTimer;
             self.reading_back = MAX((1.0f - data[i+3] / 255.0f) * (1.0f + norm_back) - norm_back, 0);
         }
     }
-//    [self.btnPresetOne setFrame:CGRectMake(33, 36, 60, 60 * 1/self.reading_left)];
 
     UIColor *current_color = [UIColor colorWithRed:self.reading_left green:self.reading_right blue:self.reading_back alpha:1.0];
     self.btnSensorStatus.backgroundColor = current_color;
-//    self.faceImageContainer.backgroundColor = current_color;
     
     if (!self.preset_one_set) {
         self.btnPresetOne.backgroundColor = current_color;
     }
     self.distance_preset_one = [self calculateColorDistanceForLeft:self.reading_left andRight:self.reading_right andBack:self.reading_back andPreset:self.btnPresetOne];
-
     
     NSLog(@"Distance %02f", self.distance_preset_one);
 
     
 }
+
+
 
 -(void) rotateFaceImageWithAtSpeed:(float)speed fromAngle:(float) startAngle toAngle:(float) endAngle andRepeat:(int) n_repeat{
     
@@ -276,49 +216,57 @@ NSTimer *rssiTimer;
     self.faceAnimation.duration = speed;
     self.faceAnimation.repeatCount = n_repeat;
     [self.faceImageContainer.layer addAnimation:self.faceAnimation forKey:@"SpinAnimation"];
-    
-//    [UIView animateWithDuration : 0.5
-//                          delay : 0
-//                        options : UIViewAnimationOptionBeginFromCurrentState
-//                     animations : (void (^)(void)) ^{
-//                         self.faceImageContainer.transform = CGAffineTransformMakeScale(scaling, scaling);
-//                         self.faceImageContainer.transform = CGAffineTransformRotate(transform, degreesToRadians(-10));
-//                     }
-//                      completion:^(BOOL finished){
-//                          self.faceImageContainer.transform = CGAffineTransformIdentity;
-//                      }];
-    
-//    [UIView animateWithDuration : 0.5
-//                          delay : 0
-//                        options : UIViewAnimationOptionBeginFromCurrentState
-//                     animations : (void (^)(void)) ^{
-//                         self.faceImageContainer.transform = CGAffineTransformMakeScale(1/scaling, 1/scaling);
-//                     }
-//                      completion:^(BOOL finished){
-//                          self.faceImageContainer.transform = CGAffineTransformIdentity;
-//                      }];
 }
-
-
-
 
 
 -(void) connectionTimer:(NSTimer *)timer
 {
-//    [btnConnect setEnabled:true];
-    
-//    [btnConnect setTitle:@"" forState:UIControlStateNormal];
-    
     if (ble.peripherals.count > 0)
     {
         [ble connectPeripheral:[ble.peripherals objectAtIndex:0]];
     }
-    else
-    {
-//        [btnConnect setTitle:@"Connect" forState:UIControlStateNormal];
+}
+
+-(void) initAudioPlayer{
+    NSString *soundFilePath = [NSString stringWithFormat:@"%@/cry2.wav", [[NSBundle mainBundle] resourcePath]];
+    
+    NSLog(@"%@",soundFilePath);
+    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: soundFilePath];
+    
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setActive: YES error: nil];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    
+    [self.audioPlayer setVolume:1.0];
+}
+
+- (void)playAudio {
+    [self.audioPlayer play];
+}
+
+- (void)playSound :(NSString *)fName :(NSString *) ext{
+    SystemSoundID audioEffect;
+    NSString *path = [[NSBundle mainBundle] pathForResource : fName ofType :ext];
+    if ([[NSFileManager defaultManager] fileExistsAtPath : path]) {
+        NSURL *pathURL = [NSURL fileURLWithPath: path];
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef) pathURL, &audioEffect);
+        AudioServicesPlaySystemSound(audioEffect);
+    }
+    else {
+        NSLog(@"error, file not found: %@", path);
     }
 }
 
+-(void) readRSSITimer:(NSTimer *)timer
+{
+    [ble readRSSI];
+}
 
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
